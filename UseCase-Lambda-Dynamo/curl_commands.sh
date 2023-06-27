32786d8bd3f8a1f2651cfd54b5059a51c3298587
sidoncloud <133084113+sidoncloud@users.noreply.github.com>
2023-06-27 18:04:13 +0400

    Update curl_commands.sh

--- UseCase-Lambda-Dynamo/curl_commands.sh
+++ UseCase-Lambda-Dynamo/curl_commands.sh
@@ -1,18 +1,28 @@
 # Write Transactions
 curl -X POST https://your-api-gateway-invoke-url \
      -d '{
-         "transaction_id":"ABC12345",
+         "transaction_id":"438891",
          "user_id":"U12345",
-         "amount_usd":300.0,
-         "creation_date":"2023-06-18",
+         "num_items":3,
+         "creation_date":"2023-06-22",
          "product_id":"B12345",
          "transaction_status":"Completed"
+     }'
+
+curl -X POST https://your-api-gateway-invoke-url \
+     -d '{
+         "transaction_id":"438892",
+         "user_id":"U42345",
+         "num_items":7,
+         "creation_date":"2023-06-22",
+         "product_id":"B1N7T",
+         "transaction_status":"On-Hold"
      }'
 
 
 # Read Transactions
 curl -X POST your-api-gateway-invoke-url \
      -d '{
-         "transaction_id":"CTABC12345"
+         "transaction_id":"438891"
      }'
 
